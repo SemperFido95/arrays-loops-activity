9137cae4ee500d14abd4729a21e419d4580f3a77
Bailey Fiedorowicz <bfiedorowicz1995@gmail.com>
2023-01-19 13:15:16 -0500

    answered all questions with my group

--- client.js
+++ client.js
@@ -7,6 +7,11 @@
  *    hobbies
  */
 
+let hobbies = ['singing', 'working out', 'gaming'];
+for (let i of hobbies) {
+    console.log(i);
+}
+console.log(hobbies.length);
 
 // Example output
 // 1. swimming
@@ -24,6 +29,19 @@
  *    is in the array
  * 4. Output the array and number of times teal was found
  */
+
+let colors = ['blue', 'orange', 'purple', 'teal'];
+
+let tealCount = 0;
+
+for (let i = 0; i < colors.length; i++) {
+    if (colors[i] === 'teal') {
+        tealCount += 1;
+    } 
+};
+
+console.log(colors);
+console.log(tealCount);
 
 
 // Example output
@@ -47,6 +65,21 @@
 // Odd 3, 7, 11
 // Even 2, 8, 4, 2
 
+let numbers = [3, 69, 20, 0, 13];
+let oddNumbers = [];
+let evenNumbers = [];
+
+for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] % 2 === 0) {
+        evenNumbers.push(numbers[i]);
+    } else {
+        oddNumbers.push(numbers[i]);
+    }
+};
+
+console.log(numbers);
+console.log(evenNumbers);
+console.log(oddNumbers);
 
 /**
  * #4 Flipping Switches
@@ -62,6 +95,19 @@
 // true, false, true, true
 // Toggled false, true, false, false
 
+let bool = [true, false, false, true, false, true, true, false];
+let toggled = [];
+
+for (let i = 0; i < bool.length; i++) {
+    if (bool[i] === true) {
+        toggled.push(false);
+    } else {
+        toggled.push(true);
+    }
+}
+
+console.table(bool);
+console.table(toggled);
 
 /**
  * #5 Remove 0's
@@ -77,3 +123,20 @@
 // Example output
 // Before loop 3, 0, 2, 8, 0, 0, 0
 // After loop 3, 0, 2, 8
+
+let num = [0, 3, 5, 0, 9, 7, 8, 0, 0];
+
+for (let i = 0; i < num.length; i++) {
+    if (num[num.length - 1] === 0) {
+        num.pop();
+    }
+}; 
+
+console.log(num);
+
+
+while (num[num.length - 1] === 0) {
+        num.pop();
+};
+
+console.log(num);
